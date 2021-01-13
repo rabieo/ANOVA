8380dbd849065aa006ecbc8c7cb139323df2e4a3
Omar Rabie <rabieo@mcmaster.ca>
2021-01-13 16:45:26 -0500

    fixed the effect issue

--- src/components/Dashboard/News.js
+++ src/components/Dashboard/News.js
@@ -10,17 +10,20 @@ const News = () => {
 
     const [newss, setNewss] = useState([])
 
-    useEffect(async () => {
-        // Run! Like go get some data from an API.
+    useEffect( () => {
+        
         let temp = [];
-
-        await firestore.collection("news").get().then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
-                // doc.data() is never undefined for query doc snapshots
-                console.log(doc.data());
-                temp.push(doc.data());
-            });
-        })
+        
+        async function fetchData() {
+            await firestore.collection("news").get().then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc) {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc.data());
+                    temp.push(doc.data());
+                });
+            })
+        }
+        fetchData();
 
         setNewss(temp);
         console.log(temp);
